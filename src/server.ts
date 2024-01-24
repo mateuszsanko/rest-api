5f6f22e907ea4e9f001c24ff86e3220cc91a140e
Mateusz Sańko <mateusz.sanko@gmail.com>
2024-01-24 14:04:06 +0100

    Add watcher, config and port handling

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,46 @@
+
+import * as dotenv from "dotenv";
+
+const result = dotenv.config();
+
+if (result.error) {
+    console.log("Error loading environment variables, aborting.")
+    process.exit(1);
+}
+
 import * as express from 'express';
 import {root} from "./routes/root";
+import {isInteger} from "./utils";
+import {parse} from "dotenv";
 
 const app = express();
 
 function setupExpress() {
-    //http://localhost:9000/
     app.route("/").get(root);
 }
 
 function startServer() {
-    app.listen(9000, () => {
-        console.log("HTTP REST API Server is now running at http://localhost:9000");
+
+    let port: number;
+
+    const portEnv = process.env.PORT,
+        portArg = process.argv[2];
+
+    if (isInteger(portEnv)) {
+        port = parseInt(portEnv);
+    }
+
+    if (!port && isInteger(portArg)) {
+        port = parseInt(portArg);
+    }
+
+    if (!port) {
+        port = 9000;
+    }
+
+    app.listen(port, () => {
+
+        console.log(`HTTP REST API Server is now running at http://localhost:${port}`);
     })
 }
 
